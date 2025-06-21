35477327f94876f93cb8c085ef5d1f471dc9bf79
romj99 <99736241+romj99@users.noreply.github.com>
2025-06-21 19:43:58 +0200

    adjusted data paths

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATA_PATHS = {
-    "landing": os.path.join(BASE_DIR, "data_zones/01_landing"),
-    "formatted": os.path.join(BASE_DIR, "data_zones/02_formatted"),
-    "exploitation": os.path.join(BASE_DIR, "data_zones/03_exploitation"),
+    "landing": os.path.join(BASE_DIR, "../data_zones/01_landing"),
+    "formatted": os.path.join(BASE_DIR, "../data_zones/02_formatted"),
+    "exploitation": os.path.join(BASE_DIR, "../data_zones/03_exploitation"),
 }
 
 MLFLOW_URI = "http://localhost:5001"
