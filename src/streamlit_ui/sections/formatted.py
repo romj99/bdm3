1e802697e0849e730e0ef7522fd866039d3150f1
romj99 <99736241+romj99@users.noreply.github.com>
2025-06-21 20:12:24 +0200

    add formatted file inspector in ui

--- src/streamlit_ui/sections/formatted.py
+++ src/streamlit_ui/sections/formatted.py
@@ -1,3 +1,24 @@
+import os
 import streamlit as st
 
-st.title("Formatted Zone")
+FORMATTED_PATH = os.environ.get("FORMATTED_ZONE")
+
+st.title("🔩 Formatted Zone")
+st.write("In this section, you can view and manage the formatted datasets stored in the Datalake (Formatted Zone).")
+
+
+st.subheader("📁 Formatted Zone Explorer")
+formated_root = FORMATTED_PATH
+
+for dirpath, dirnames, filenames in os.walk(formated_root):
+    level = dirpath.replace(formated_root, "").count(os.sep)
+    indent = "— " * level
+    relative_dir = os.path.relpath(dirpath, formated_root)
+
+    if relative_dir != ".":
+        with st.expander(f"{indent}{relative_dir}/", expanded=False):
+            if filenames:
+                for file in filenames:
+                    st.markdown(f"- `{file}`")
+            else:
+                st.markdown("*_No files in this folder_*")
